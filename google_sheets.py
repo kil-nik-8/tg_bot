b0b7f7e338e1487ce61d0698b5128a4bb3362830
Nikita <145984876+kil-nik-8@users.noreply.github.com>
2025-05-23 17:48:45 +0300

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -3,28 +3,53 @@ import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
 from core.config import SHEET_KEY
 
-# TODO вынести в отдельный файл
 CREDENTIALS_FILE = "credentials.json"
 SPREADSHEET_ID = SHEET_KEY
 
+
+
 rename_dct = {
-    "ФИО" : "party_rk",
-    "Дата заявки" : "open_dt",
-    "Страна поездки" : "Страна",
-    "Город поездки" : "Город_text",
-    "Месяц отправления" : "Месяц отправления",
-    "Звездность отеля" : "Звездность",
-    "Большая территория": "Большая территория",
-    "Наличие аквапарка": "Наличие аквапарка",
-    "Все включено": "Все включено",
-    "Активная анимация": "Активная анимация",
-    "Песчаный пляж": "Пляж песчаный",
-    "Много зелени": "Много зелени",
-    "Красивая природа": "Красивая природа",
-    "Первая линия": "Первая линия",
-    "Рядом большой город": "Рядом большой город",
-    "Размещение с животными": "Размещение с животными",
-    "Хорошая транспортная доступность": "Хорошая транспортная доступность",
+    "Дата заявки": "open_dt",
+    "Страна поездки": "Страна",
+    "ФИО": "party_rk",
+    "Месяц отправления": "Месяц отправления",
+    "Тип клиента": "Тип клиента",
+    "Звезды отеля": "stars",
+    "Близость к аэропорту": "proximity_to_airport",
+    "Близость к крупному городу": "close_to_major_city",
+    "Транспортная доступность": "good_transport_accessibility",
+    "Первая береговая линия": "first_coastline",
+    "Вторая береговая линия": "second_coastline",
+    "Трансфер к пляжу": "shuttle_service_to_beach",
+    "Близость к пляжу": "remote_from_beach",
+    "Крупный отель": "large_hotel",
+    "Аквапарк": "water_park",
+    "Открытые бассейны": "outdoor_pools",
+    "Крытые бассейны": "indoor_pools",
+    "Подогреваемые бассейны": "heated_pools",
+    "Спа-центр": "spa_center",
+    "Тренажерный зал": "gym",
+    "Система 'Все включено'": "all_inclusive_system",
+    "Полный пансион": "full_board",
+    "Полупансион": "half_board",
+    "Завтрак включен": "breakfast_included",
+    "Бесплатные рестораны": "free_restaurants",
+    "Платные рестораны": "paid_restaurants",
+    "Круглосуточный бар": "twenty_four_hour_bar",
+    "Детский аквапарк": "kids_water_park",
+    "Отель для взрослых": "adults_only_hotel",
+    "Бесплатный Wi-Fi": "free_wifi",
+    "Мини-бар": "mini_bar",
+    "Частный пляж": "private_beach",
+    "Бесплатные шезлонги": "free_sunbeds_umbrellas",
+    "Песчаный пляж": "sandy_beach",
+    "Галечный пляж": "pebble_beach",
+    "Бесплатная парковка": "free_parking",
+    "Развлечения для взрослых": "adult_entertainment",
+    "Детские развлечения": "kids_entertainment",
+    "Озелененная территория": "abundant_greenery",
+    "Размещение с животными": "pet_friendly_accommodation",
+    "Кондиционер": "air_conditioning"
 }
 
 
